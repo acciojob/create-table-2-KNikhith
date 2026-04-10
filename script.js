6a54146f02c949f20211fab5491d21b6cb2120b8
KNikhith <nikhithkotluri@gmail.com>
2026-04-10 11:01:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+	let rn=Number(prompt("Input number of rows"));
+	let cn=Number(prompt("Input number of columns"));
+	if(isNaN(rn)||isNaN(cn)){
+		return;
+	}
+	let table=document.getElementById("myTable");
+	table.innerHTML="";
+	for(let i=0;i<rn;i++){
+		let row=table.insertRow();
+		for(let j=0;j<cn;j++){
+			let cell=row.insertCell();
+			cell.textContent=`Row-${i} Column-${j}`;
+		}
+	}
+	
   
 }
